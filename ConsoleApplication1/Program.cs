b1a51d0551c0f3ef1ce55efd707e1d788e584d61
suchitadolare <64899610+suchitadolare@users.noreply.github.com>
2020-05-07 12:01:43 +0530

    added new files

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            c = a + b;
+            Console.WriteLine("C=" + c);
+            Console.ReadLine();
+    
+        }
+    }
+}
